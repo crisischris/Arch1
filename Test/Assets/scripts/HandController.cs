0cd4cbddd155948c681469cf93940e8e8105338f
chris nelson <christopherthomasnelson@gmail.com>
2018-01-07 22:43:14 -0800

    Initial commit

---
+++ Test/Assets/scripts/HandController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HandController : MonoBehaviour
+{
+
+
+    public SteamVR_TrackedObject trackedObj;
+    public SteamVR_Controller.Device device;
+
+
+    //public doorController DoorController;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+
+
+        // Update is called once per frame
+
+
+
+    }
+
+    void update()
+    {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            device.TriggerHapticPulse(3000);
+        }
+
+
+
+        }
+
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Model"))
+        {
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                device.TriggerHapticPulse(3000);
+                Debug.Log("CLicked");
+            }
+
+
+            //public void OnTriggerEnter(Collider col)
+            //{
+            //    if (col.gameObject.CompareTag("DOOR-KITCHEN"))
+            //    {
+            //        device.TriggerHapticPulse(3000);
+            //
+            //    }
+            //}
+
+
+            // void OnTriggerStay(Collider col)
+            // {
+            //     if (col.gameObject.CompareTag("DOOR-KITCHEN"))
+            //     {
+            //         if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+            //         {
+            //             DoorController.OpenKitchen();
+            //         }
+            //     }
+            // }
+        }
+    }
+}
---
+++ Test/Assets/scripts/doorController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorController : MonoBehaviour {
+
+    public GameObject KitchenDoor;
+    public GameObject BarDoorLong;
+    public GameObject BarDoorShort;
+
+    public Vector3 startPosKitchen;
+    public Vector3 startPosBarLong;
+    public Vector3 startPosBarShort;
+
+    public Vector3 endPosKitchen;
+    public Vector3 endPosBarLong ;
+    public Vector3 endPosBarShort;
+
+    // Use this for initialization
+    void Start () {
+
+       startPosKitchen = KitchenDoor.transform.position;
+       startPosBarLong = BarDoorLong.transform.position;
+       startPosBarShort = BarDoorShort.transform.position;
+
+         endPosKitchen = new Vector3(4f,KitchenDoor.transform.position.y, KitchenDoor.transform.position.z);
+         endPosBarLong = new Vector3(BarDoorLong.transform.position.x, BarDoorLong.transform.position.y,5.51f);
+         endPosBarShort = new Vector3(4.1f, BarDoorShort.transform.position.y, BarDoorShort.transform.position.z);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void OpenKitchen()
+    {
+        KitchenDoor.transform.position = Vector3.Lerp(startPosKitchen, endPosKitchen, 3);
+    }
+}
